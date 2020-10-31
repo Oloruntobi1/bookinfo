82b49f8fb0bbae64b03f2d49974b84685a23846d
Oloruntobi1 <blgntobi@gmail.com>
2020-10-31 01:31:34 +0100

    written the server code

---
+++ book_merce/server/server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/Oloruntobi1/bookinfo/book_merce/bookmerce"
+	"github.com/gofrs/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const port = ":50051"
+
+type server struct {
+	pb.UnimplementedBookMerceServer
+	bookMap map[string]*pb.Book
+}
+
+func (s server) AddBook(ctx context.Context, in *pb.Book) (*pb.BookID, error) {
+
+	out, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal,
+			"Error while generating Book ID", err)
+	}
+	in.Id = out.String()
+	if s.bookMap == nil {
+		s.bookMap = make(map[string]*pb.Book)
+	}
+	s.bookMap[in.Id] = in
+	return &pb.BookID{Value: in.Id}, status.New(codes.OK, "").Err()
+
+}
+
+// GetBook implements bookmerce.GetBook
+func (s *server) GetBook(ctx context.Context, in *pb.BookID) (*pb.Book, error) {
+	value, exists := s.bookMap[in.Value]
+		if exists {
+			return value, status.New(codes.OK, "").Err()
+		}
+		return nil, status.Errorf(codes.NotFound, "Book does not exist.", in.Value)
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+	log.Fatalf("failed to listen: %v", err)
+	}
+		s:= grpc.NewServer()
+		pb.RegisterBookMerceServer(s, &server{})
+	
+	log.Printf("Starting gRPC listener on port " + port)
+		if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+}
+
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/Oloruntobi1/bookinfo
 go 1.15
 
 require (
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.1 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	github.com/gofrs/uuid v3.3.0+incompatible
+	github.com/golang/protobuf v1.4.1
+	google.golang.org/grpc v1.27.0
+	google.golang.org/protobuf v1.25.0
 )
